b9c33152d0579c3b3bc6984c960ad13672172afb
Matt Cowan <matt@mattbcowan.com>
2022-01-27 06:30:05 -0600

    feat: Fetch user data from database

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+import { db } from "./firebase";
+import { ref, onValue } from "firebase/database";
+
 function App() {
-  return <div className="App">Invoice App</div>;
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getUser = (user) => {
+    try {
+      setLoading(true);
+      let userRef = ref(db, "/users/" + user);
+
+      onValue(userRef, (snapshot) => {
+        const data = snapshot.val();
+        setUser(data);
+        setLoading(false);
+      });
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  useEffect(() => {
+    getUser("USR1234");
+  }, []);
+
+  return (
+    <div className="App">
+      {error && <p>Error: {error}</p>}
+      {loading && <p>loading...</p>}
+      {!loading && user !== null && <p>{user.name}</p>}
+    </div>
+  );
 }
 
 export default App;
