2cb4404f69c19d8345a31df86cab6a1d7ea88490
Matt Cowan <matt@mattbcowan.com>
2022-02-15 16:41:04 -0600

    refactor: reduced formatting code

--- src/formatting.js
+++ src/formatting.js
@@ -1,26 +1,10 @@
-import { db, auth } from "./firebase";
-import { get, ref } from "firebase/database";
-
 const padLeadingZeros = (num, size) => {
   var s = num + "";
   while (s.length < size) s = "0" + s;
   return s;
 };
 
-export const createInvoiceNumber = async () => {
-  const invoicesRef = ref(db, `users/${auth.currentUser.uid}/invoices`);
-  let invoiceNumber = 1;
-  await get(invoicesRef)
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        const numberOfInvoices = Object.keys(snapshot.val()).length;
-        return (invoiceNumber = numberOfInvoices + 1);
-      } else {
-        return (invoiceNumber = 1);
-      }
-    })
-    .catch((err) => console.error(err));
-
-  invoiceNumber = `XM${padLeadingZeros(invoiceNumber, 4)}`;
+export const createInvoiceNumber = (numOfInvoices) => {
+  const invoiceNumber = `XM${padLeadingZeros(numOfInvoices, 4)}`;
   return invoiceNumber;
 };
