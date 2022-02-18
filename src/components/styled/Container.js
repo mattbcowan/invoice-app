420de8d179c1f535b5680cfc6f2417fa870cdde7
Matt Cowan <matt@mattbcowan.com>
2022-02-18 05:48:47 -0600

    style: created container component

---
+++ src/components/styled/Container.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  max-width: 730px;
+  margin: 2em;
+
+  @media (min-width: 768px) {
+    margin: 0 auto;
+    margin-top: 2em;
+  }
+`;
+
+export default Container;
