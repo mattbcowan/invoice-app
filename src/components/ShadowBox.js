c42d96d2ffe40504b81f20e7b2b9967774ab256d
Matt Cowan <matt@mattbcowan.com>
2022-02-18 05:40:44 -0600

    refactor: Removed styled system from shadowbox

--- src/components/ShadowBox.js
+++ src/components/ShadowBox.js
@@ -1,18 +1,14 @@
 import styled from "styled-components";
-import { layout, space, flexbox } from "styled-system";
-import theme from "../theme/theme";
 
-const ShadowBox = styled.div(
-  {
-    boxSizing: "border-box",
-    minWidth: 0,
-    background: "#ffffff",
-    boxShadow: "0px 10px 10px -10px rgba(72, 84, 159, 0.100397)",
-    borderRadius: theme.radii[2],
-  },
-  space,
-  layout,
-  flexbox
-);
+const ShadowBox = styled.div`
+  box-sizing: border-box;
+  min-width: 0;
+  background: #ffffff;
+  box-shadow: 0px 10px 10px -10px rgba(72, 84, 159, 0.100397);
+  border-radius: ${({ theme }) => theme.radii[2]};
+  padding: 2em;
+  margin-bottom: ${(props) => props.marginBottom};
+  margin-top: ${(props) => props.marginTop};
+`;
 
 export default ShadowBox;
